897b49c2e3791ee28ed4be61ff1753740708ce37
Susana Marquina <smarq86@gmail.com>
2022-07-13 21:20:12 -0700

    Built password generator on Scrimba platform

---
+++ index.js
@@ -0,0 +1,29 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let passwordLength = 12;
+let passwordOne = document.getElementById("password-one");
+let passwordTwo = document.getElementById("password-two");
+
+function getRandomCharacter() {
+    let randomChar = Math.floor(Math.random() * characters.length);
+    return characters[randomChar];
+}
+
+function generateRandomPassword() {
+    let randomPassword = '';
+    for (let i =0; i < passwordLength; i++) {
+        randomPassword += getRandomCharacter();
+        passwordOne.innerHTML = randomPassword;
+        passwordTwo.innerHTML = randomPassword;
+    }
+    return randomPassword
+}
+
+
+
+
+
+
+
+
